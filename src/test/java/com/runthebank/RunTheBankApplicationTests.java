462389d419859650e9c7b30db711a515e057f53a
jacksonbispo <jackson.novaes@live.com>
2024-01-12 11:34:15 -0300

    Init commit create a repository

---
+++ src/test/java/com/runthebank/RunTheBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runthebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTheBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
